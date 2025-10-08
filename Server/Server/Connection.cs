89666c9fad26c1cd2aceec352e9abceeb1601034
SnakeGuitar <snakersito08@gmail.com>
2025-10-07 21:52:42 -0600

    Client and Server: Basic structure 2
    Database might change.

---
+++ Server/Server/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace Server
+{
+    class Connection
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Server.Service1)))
+            {
+                try
+                {
+                    host.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.ReadLine();
+                }
+                Console.WriteLine("Service is running...");
+                Console.WriteLine("Press Enter to terminate service.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
