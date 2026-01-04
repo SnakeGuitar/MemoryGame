60be9fa2402d6f50944293cc2e2b7fb5a47e5025
SnakeGuitar <snakersito08@gmail.com>
2026-01-03 18:22:01 -0600

    Server.Test: Updated helper classes

--- Server/Test/Helpers/DbContextMockFactory.cs
+++ Server/Test/Helpers/DbContextMockFactory.cs
@@ -1,10 +1,7 @@
 ﻿using Moq;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test.Helpers
 {
@@ -16,7 +13,9 @@ namespace Test.Helpers
             mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
             mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
             mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+            mockSet.As<IEnumerable<T>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+            mockSet.Setup(m => m.Include(It.IsAny<string>())).Returns(mockSet.Object);
             return mockSet;
         }
     }
--- Server/Test/Helpers/MockLobbyCallback.cs
+++ Server/Test/Helpers/MockLobbyCallback.cs
@@ -4,54 +4,40 @@ using System.Collections.Generic;
 
 namespace Test.Helpers
 {
+    /// <summary>
+    /// Dummy IGameLobbyCallback for testing.
+    /// </summary>
     public class MockLobbyCallback : IGameLobbyCallback
     {
-        public void CardFlipped(int cardIndex, int cardIndex2)
-        {
-        }
-
-        public void GameFinished(string winnerName)
-        {
-        }
-
-        public void GameStarted(List<CardInfo> gameBoard)
-        {
-        }
-
-        public void HideCards(int cardIndex, int cardIndex2)
-        {
-        }
-
-        public void PlayerJoined(string playerName, bool isGuest)
-        {
-        }
-
-        public void PlayerLeft(string playerName)
-        {
-        }
+        public bool MessageReceived { get; private set; }
+        public string LastMessage { get; private set; }
 
         public void ReceiveChatMessage(string senderName, string message, bool isNotification)
         {
+            MessageReceived = true;
+            LastMessage = message;
         }
 
-        public void SetCardsAsMatched(int cardIndex1, int cardIndex2)
-        {
-        }
+        public void PlayerJoined(string playerName, bool isGuest) { }
 
-        public void ShowCard(int cardIndex, string imageIdentifier)
-        {
-        }
+        public void PlayerLeft(string playerName) { }
 
-        public void UpdatePlayerList(LobbyPlayerInfo[] players)
-        {
-        }
+        public void UpdatePlayerList(LobbyPlayerInfo[] players) { }
 
-        public void UpdateScore(string playerName, int newScore)
-        {
-        }
+        public void GameStarted(List<CardInfo> gameBoard) { }
 
-        public void UpdateTurn(string playerName, int turnTimeInSeconds)
-        {
-        }
+        public void UpdateTurn(string playerName, int turnTimeInSeconds) { }
+
+        public void ShowCard(int cardIndex, string imageIdentifier) { }
+
+        public void HideCards(int cardIndex1, int cardIndex2) { }
+
+        public void CardFlipped(int cardIndex, int cardIndex2) { }
+
+        public void SetCardsAsMatched(int cardIndex1, int cardIndex2) { }
+
+        public void UpdateScore(string playerName, int newScore) { }
+
+        public void GameFinished(string winnerName) { }
     }
 }
