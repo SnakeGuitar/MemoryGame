b14ad5e22a7a87c5606278feb3a013d4286564b2
SnakeGuitar <snakersito08@gmail.com>
2026-01-10 10:50:19 -0600

    Create WpfExtensions.cs

---
+++ Client/Client/Helpers/WpfExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Client.Helpers
+{
+    public static class WpfExtensions
+    {
+        public static async void SafeExecute(this Button button, Func<Task> action, Window owner)
+        {
+            if (button == null)
+            {
+                return;
+            }
+
+            button.IsEnabled = false;
+            var originalCursor = Mouse.OverrideCursor;
+            Mouse.OverrideCursor = Cursors.Wait;
+
+            try
+            {
+                await action();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.Handle(ex, owner);
+            }
+            finally
+            {
+                if (Application.Current.MainWindow == owner)
+                {
+                    button.IsEnabled = true;
+                }
+                Mouse.OverrideCursor = originalCursor;
+            }
+        }
+    }
+}
