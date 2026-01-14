09eae0a25fe5943b278a0681efcbffe795e55abb
SnakeGuitar <snakersito08@gmail.com>
2026-01-14 13:54:03 -0600

    Server: Now it is executable

---
+++ Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using Server.LobbyService;
+using Server.SessionService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost userHost = new ServiceHost(typeof(UserService));
+            ServiceHost lobbyHost = new ServiceHost(typeof(GameLobbyService));
+
+            try
+            {
+                userHost.Open();
+                lobbyHost.Open();
+
+                Console.WriteLine("Server started succesfully.");
+                Console.WriteLine("Service running. Type [ENTER] to stop.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong.");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (userHost.State == CommunicationState.Opened)
+                    userHost.Close();
+
+                if (lobbyHost.State == CommunicationState.Opened)
+                    lobbyHost.Close();
+            }
+        }
+    }
+}
